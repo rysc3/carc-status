f02b91e55db9408794cd7f8abcc5769bb6d114cc
Ryan Scherbarth <ry.scherbarth@gmail.com>
2024-05-28 22:23:55 -0600

    working ssh connection

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -2,7 +2,29 @@ class NodesController < ApplicationController
   before_action :set_node, only: [:show, :edit, :update, :destroy]
 
   def index
-    @nodes = Node.all
+    host = '129.24.245.8'
+    username = 'ryan'
+    private_key = 'app/assets/keys/server_key.pem'
+
+    begin
+      Net::SSH.start(host, username, keys: [private_key]) do |ssh|
+        # Get hostname
+        result_hostname = ssh.exec!("hostname")
+        @hostname = result_hostname&.strip  # Use &. to safely call strip on result if it's not nil
+
+        # Get system time
+        result_time = ssh.exec!("date")
+        @system_time = result_time&.strip
+      end
+    rescue Net::SSH::AuthenticationFailed
+      flash[:error] = "Authentication failed. Please check your credentials."
+      @hostname = nil
+      @system_time = nil
+    rescue StandardError => e
+      flash[:error] = "Failed to fetch hostname or system time: #{e.message}"
+      @hostname = nil
+      @system_time = nil
+    end
   end
 
   def show
